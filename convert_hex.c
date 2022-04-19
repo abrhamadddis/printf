2a16976b0ba43110da18dcba9583bb39ce876c40
melakualehgn <melakualehegn34@gmail.com>
2022-04-19 04:32:24 -0700

    convert_hex.c hot fix

--- convert_hex.c
+++ convert_hex.c
@@ -61,3 +61,28 @@ unsigned int convert_x(va_list args, buffer_t *output,
  *
  * Return: The number of bytes stored to the buffer.
  */
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len)
+{
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0X";
+
+	if (len == LONG)
+		num = va_arg(args, unsigned long);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+
+	if (!(num == 0 && prec == 0))
+		ret += convert_ubase(output, num, "0123456789ABCDEF",
+				flags, wid, prec);
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
+}
